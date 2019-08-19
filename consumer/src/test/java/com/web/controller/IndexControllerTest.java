0f22f56ffef92babe6de9a01cd2d75e0497f07d1
陈海洋 <1559843332@qq.com>
2019-08-19 20:24:14 +0800

    修改zookeeper注册地址

--- consumer/src/test/java/com/web/controller/IndexControllerTest.java
+++ consumer/src/test/java/com/web/controller/IndexControllerTest.java
@@ -2,17 +2,31 @@ package com.web.controller;
 
 import com.entity.ErrorLog;
 import com.service.ErrorLogService;
+import com.service.UserInfoService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class IndexControllerTest {
+
+
+    @Reference(version = "1.0.0",check = false)
+    private UserInfoService userInfoService;
+
+    @Test
+    public void aaa(){
+        System.out.println(userInfoService.getUserInfo(1));
+    }
+
+
+
 
     @Reference(version = "1.0.1")
     private ErrorLogService errorLogService;
@@ -23,4 +37,7 @@ public class IndexControllerTest {
         errorLog.setName("error");
         System.out.println(errorLogService.sendMsg(errorLog));
     }
+
+
+
 }
